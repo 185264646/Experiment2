2497e12ec902353ac905d7927d09333125eb7e54
root <root@localhost>
2020-10-23 23:56:27 +0800

    implement suffix detection better

--- 2.4.1-new.c
+++ 2.4.1-new.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#define isodigit(x) (isdigit(x) && (x) != '8' && (x) != '9'
-static is_valid_suffix(const char *);  // receive a const char * parameter which points to the first charactor of the suffix string.
+#define isodigit(x) (isdigit(x) && (x) != '8' && (x) != '9')
+static _Bool is_valid_suffix(const char *);  // receive a const char * parameter which points to the first charactor of the suffix string.
 _Bool is_valid(const char *);
 int main(void)
 {
@@ -27,7 +27,7 @@ _Bool is_valid(const char *str)
 				return 0;
 			for (int i = 2, end = strlen(str); i < end; i++)
 				if (!isxdigit(str[i]))
-					if (str[i] == 'l' || str[i] == 'L' && str[i + 1] == '\0')
+					if (is_valid_suffix(str + i))
 						return 1;
 					else
 						return 0;
@@ -38,7 +38,7 @@ _Bool is_valid(const char *str)
 				return 1;
 			for (int i = 1, end = strlen(str); i < end; i++)
 				if (!isodigit(str[i]))
-					if (str[i] == 'l' || str[i] == 'L' && str[i + 1] == '\0')
+					if (is_valid_suffix(str + i))
 						return 1;
 					else
 						return 0;
@@ -49,12 +49,12 @@ _Bool is_valid(const char *str)
 	{
 		for (int i = 0, end = strlen(str); i < end; i++)
 			if(!isdigit(str[i]))
-				if (str[i] == 'l' || str[i] == 'L' && str[i + 1] == '\0')
+				if (is_valid_suffix(str + i))
 					return 1;
 				else
 					return 0;
 	}
-	else if (str[1] == 'l' || str[1] == 'L' && str[2] == '\0')
+	else if (is_valid_suffix(str + 1))
 		return 1;
 	else
 		return 0;
@@ -62,18 +62,19 @@ _Bool is_valid(const char *str)
 }
 
 static _Bool is_valid_suffix(const char *suffix)
-{	
-	for (int i = 0; i < 3 && str[i]; i++)
-		str[i] = tolower(str[i]);
-	switch (suffix[0])
+{
+	char temp[4]="";
+	for (int i = 0; i < 3 && suffix[i]; i++)
+		temp[i] = tolower(suffix[i]);
+	switch (temp[0])
 	{
 		case 'u':
-			switch (suffix[1])
+			switch (temp[1])
 			{
 				case 'l':
-					switch (suffix[2])
+					switch (temp[2])
 					{
-						case 'l': return suffix[3] == '\0' ? 1 : 0
+						case 'l': return temp[3] == '\0' ? 1 : 0;
 						case '\0': return 1;
 						default: return 0;
 					}
@@ -85,20 +86,22 @@ static _Bool is_valid_suffix(const char *suffix)
 			}
 			
 		case 'l':
-			switch (suffix[1])
+			switch (temp[1])
 			{
 				case 'l':
-					switch (suffix[2])
+					switch (temp[2])
 					{
-						case 'u': return suffix[3] == '\0' ? 1 : 0;
+						case 'u': return temp[3] == '\0' ? 1 : 0;
 						case '\0': return 1;
 						default: return 0;
 					}
 				
 				case 'u':
-					return suffix[2] == '\0' ? 1 : 0;
+					return temp[2] == '\0' ? 1 : 0;
 				
 				case '\0':
 					return 1;
 			}
 	}
+	return 0;
+}
